beab6a94ce4b2a0f9bdb2a0b87de112e180d8bab
bboytchev63 <bboytchev@hotmail.com>
2026-05-29 14:58:30 +0300

    #7 Lists

--- main.py
+++ main.py
@@ -48,11 +48,46 @@
 #         count += 1
 # print("Count of h in Python is ", count)
 
-for i in range(1, 6):
-    print(i)
-    if i == 3:
-        break
+# for i in range(1, 6):
+#     print(i)
+#     if i == 3:
+#         break
+
+# #7 – Списки (list). Функции и их методы
+# print("#7  Списки (list). Функции и их методы")
+# nums = [1, 2, 3, 4, 5,2,11,8]
+# print(nums)
+# nums[0] = True
+# print(nums)
+# nums.append(6)
+# print(nums)
+
+# nums2 = [7, 8, 9, 10,[True,False]]
+# print(nums2)
+
+# nums.sort()
+# print(nums)
+
+# nums.remove(2)
+# print(nums)
+# nums.pop(0)
+# print(nums)
+
+# nums = [1, 2, 3, 4, 5,1.5,True, "50"]
+# print(nums)
+# for i in nums:
+#     print(i, type(i))
+
+n = input("Enter length")
+userlist = []
+i = 0
+while i < int(n):
+    userlist.append(input("Enter element: "))
+    i += 1
+print(userlist)
 
 
+         
+          
 
-
+          
