c8213b057945112ad0f73fa310dcfacd1732b2fc
bboytchev63 <bboytchev@hotmail.com>
2026-05-29 14:19:33 +0300

    Start # 6 Циклы и операторы

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-print("Rakia Burgas63")
-print("Rakia, 21, 'Lv'")
-print("Rakia, 21,'Lv',sep='-')")
-print("Rakia", "21", "Lv", sep="-", end="!")
-print("First line\nSecond line \n \t Third line")
-#4 – Переменные и типы данных
-print("#4 – Переменные и типы данных")
-number = 5
-print("Number = ", number)
-numberSTR = "10"
-print("NumberSTR = ", numberSTR)
-print("Number + NumberSTR = ", number + int(numberSTR))
+# print("Rakia Burgas63")
+# print("Rakia, 21, 'Lv'")
+# print("Rakia, 21,'Lv',sep='-')")
+# print("Rakia", "21", "Lv", sep="-", end="!")
+# print("First line\nSecond line \n \t Third line")
+# #4 – Переменные и типы данных
+# print("#4 – Переменные и типы данных")
+# number = 5
+# print("Number = ", number)
+# numberSTR = "10"
+# print("NumberSTR = ", numberSTR)2
+# print("Number + NumberSTR = ", number + int(numberSTR))
 
- #5 – Условные операторы
-print("#5 – Условные операторы")
-user_data = int(input("Enter number: "))
-if user_data > 5:
-   print("Number is bigger than 5")
+# #5 – Условные операторы
+# print("#5 – Условные операторы")
+# user_data = int(input("Enter number: "))
+# if user_data > 5:
+#    print("Number is bigger than 5")
+#    if user_data > 10:
+#          print("Number is bigger than 10")
+
+isHappy = True
+
+if isHappy:
+    print("I am happy")
+else:
+    print("I am not happy")
+
+
